6c78f5fd65ee0eb13eff80c7966a2efa5551b639
Joan Galmés Riera <joan.galmes@gmail.com>
2023-05-30 11:04:20 +0200

    Eliminat module federation de mf-grupscooperatius i mf-webiesmanacor

--- src/components/common/AppsMenu.vue
+++ src/components/common/AppsMenu.vue
@@ -17,8 +17,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import { Todo, Meta } from './models';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'ExampleComponent',
